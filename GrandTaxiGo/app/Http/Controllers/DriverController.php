704bef468e5725c635b417b967e4f16c83ded92e
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-03-01 00:37:39 +0000

    Ajout de la fonctionnalité permettant aux chauffeurs de changer leur disponibilite.

--- GrandTaxiGo/app/Http/Controllers/DriverController.php
+++ GrandTaxiGo/app/Http/Controllers/DriverController.php
@@ -8,48 +8,11 @@ use Illuminate\Support\Facades\Auth;
 
 class DriverController extends Controller
 {
-public function index(){
-    $availability = User::where('id', auth()->id())->first()->is_available;
-    // dd($availability);
-    $profile=user::where('id',auth()->id())->get();
+
+
+
+
+
    
-    return view('/driver/availability',compact('availability','profile'));
-}
-
-
-public function changeAvailibilty(){
     
-$user=User::where('id',auth()->id())->first();
-if($user){
-    if($user->is_available==true){
-        $user->update(['is_available'=>false]);
-    }
-    elseif($user->is_available==false){
-        $user->update(['is_available'=>true]);
-    }
-}
-    return redirect()->route('show.availability');
-   
-}
-
-    public function filteredByLocationAndAvailibility(Request $request){
-        $location=$request->location;
-    
-        $drivers = User::where('is_available', true)
-        ->where('location', $location)
-        ->get();
-        $profile=user::where('id',auth()->id())->get();
-
-    return view('passenger/reservation',compact('drivers','profile'));
-    
-    }
-
-
-
-    // public function showProfile(){
-    //     $profile=user::where('id',auth()->id())->get();
-    //     // dd($profile);
-   
-    //     return view('layouts.driver',compact('profile'));
-    // }
 }
